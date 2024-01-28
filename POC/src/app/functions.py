f7f308c0719d2a73dc6ae36e03b7140501b7b6a2
aditya <meenakshi.bhtt@gmail.com>
2024-01-28 10:44:36 +0530

    text summarization module added

--- POC/src/app/functions.py
+++ POC/src/app/functions.py
@@ -3,6 +3,24 @@ import PyPDF2
 import re
 from PyPDF2 import PdfReader
 
+key = "6d45ac78cd7e4cda97822052bdd170e2"
+endpoint ="https://hirebolt.cognitiveservices.azure.com/"
+
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
+
+def get_text(filepath):
+    reader = PdfReader(filepath)
+    content_info = []
+    no_of_pages = len(reader.pages)
+    
+    for i in range(no_of_pages):
+        page = reader.pages[i]
+        content_info.append(page.extract_text())
+    
+    return content_info
+
 # To get the phone number from resume
 def get_phone_number(filepath: str) -> str:
     reader = PdfReader(filepath)
@@ -47,3 +65,24 @@ def get_email_id(filepath: str) -> str:
         if results:
             for match in results:
                 return match
+
+# This example requires environment variables named "LANGUAGE_KEY" and "LANGUAGE_ENDPOINT"
+
+
+
+
+def sample_abstractive_summarization(document) :
+
+    text_analytics_client = TextAnalyticsClient(
+        endpoint=endpoint,
+        credential=AzureKeyCredential(key),
+    )
+    poller = text_analytics_client.begin_abstract_summary(document)
+    abstract_summary_results = poller.result()
+    result_summary=[]
+    for result in abstract_summary_results:
+        if result.kind == "AbstractiveSummarization":
+            for summary in result.summaries:
+                result_summary.append(summary.text)
+
+    return result_summary
--- POC/src/app/main.py
+++ POC/src/app/main.py
@@ -3,11 +3,17 @@ Author -Aditya Bhatt 8:00 AM 28/01/2023
 Objective-
 1.Create a application for resume selection
 '''
+filepath="C:/Users/aditya/Desktop/2024/HireBolt.AI/POC/Aditya Bhatt CV.pdf"
+#Import Modules
 from functions import get_phone_number
 from functions import get_email_id
-filepath="C:/Users/aditya/Desktop/2024/HireBolt.AI/POC/Aditya Bhatt CV.pdf"
+from functions import sample_abstractive_summarization
+from functions import get_text
+#Call the functions
 print(get_phone_number(filepath))
 print(get_email_id(filepath))
+text=get_text(filepath)
+print(sample_abstractive_summarization(document=text))
 
 
 
